499690af7194dca3df8e2ef3f55afaf5d88e97fe
Connor Jordan <connor.samuel.jordan@gmail.com>
2023-11-02 09:12:07 -0700

    final castaway push so code is same

--- test.java
+++ test.java
@@ -115,12 +115,11 @@ public class test {
         int[] score = {72, 85, 82, 96, 90, 96, 88};
         String[] gender = {"M", "M", "M", "F", "F", "M", "F"};
         MyLinkedList<Castaway> castaways = new MyLinkedList<Castaway>();
-        
         for(int i = 0; i < gender.length; i++) {
                     castaways.insertSorted(new Castaway(lastName[i], firstName[i], score[i]
                             , gender[i]));
         }
-        for(int i = 0; i < gender.length; i++) {
+        for(int i = 0; i < gender.length-1; i++) {
             System.out.println(castaways.get(i).toString());
         }   
     }
